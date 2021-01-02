4b7f82f379b9f80f3517cfd6e53cde8b6849be5f
Ahmad Alwareh <55667622+ahmadalwareh@users.noreply.github.com>
2021-01-02 18:29:46 +0200

    Update DBCreation.sql

--- DBCreation.sql
+++ DBCreation.sql
@@ -5,3 +5,8 @@ CREATE TABLE Persons (
     Address varchar(255),
     City varchar(255)
 );
+
+CREATE TABLE new_table_name AS
+    SELECT column1, column2,...
+    FROM existing_table_name
+    WHERE ....;
